744a6e49c43459aa52961b26a63c84c5a8c54637
carnae <samarbaskar02@gmail.com>
2024-10-28 00:57:17 +0530

    add terrain visualization via 2d array

--- main.py
+++ main.py
@@ -9,13 +9,23 @@ def LineGen(line_length):
         arr = [height] * 4
         if height < 9:
             arr.append(height + 1)
-        if height > 1: 
+        if height > 0: 
             arr.append(height - 1)
         
         height = random.choice(arr)
         line += str(height)
     return line
 
+def ArrayConvert(line):
+    #uses vertical 2d array represebtation
+    line_length = len(line)
+    terrain = [[0 for _ in range(10)] for _ in range(line_length)]
+    for position in range(line_length):
+        terrain[position][int(line[position])] = 1
+    
+
 curr = time.time()
-print(LineGen(1000))
+line = LineGen(10000)
+ArrayConvert(line)
+
 print(time.time() - curr)
