4d8e2dea1fd87de85e1164dec2a2b9b7329d15d8
carnae <samarbaskar02@gmail.com>
2024-10-28 02:15:24 +0530

    create drawing functionality

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 import time
 from pynput import keyboard
+import pyautogui
 
 def LineGen(line_length):
     line = str(random.randint(2, 5))
     height = int(line)
     
-    for i in range(line_length):
+    for _ in range(line_length - 1):
         arr = [height] * 4
         if height < 9:
             arr.append(height + 1)
@@ -18,43 +19,67 @@ def LineGen(line_length):
     return line
 
 def ArrayConvert(line):
-    # Uses vertical 2D array representation
     line_length = len(line)
     terrain = [[0 for _ in range(10)] for _ in range(line_length)]
     for position in range(line_length):
         terrain[position][int(line[position])] = 1
-    return terrain  # Return the terrain array
+    return terrain
 
 def slice_terrain(terrain, pos):
     return terrain[max(0, pos-10):pos]
 
-def terrain_draw(terrain):
-    # Draw the terrain slice
-    for row in terrain:
-        print("".join(str(x) for x in row))
-    print("--------------")
+def terrain_draw(terrain, left_x, left_y):
+    pyautogui.moveTo(left_x, left_y + (10 - terrain[0].index(1)) * 40)
+    for node in terrain:
+        height = node.index(1)
+        pyautogui.mouseDown()
+        pyautogui.moveTo(left_x, left_y + (10 - height) * 40)
+        pyautogui.mouseUp()
+        left_x += 100
 
 def on_press(key, terrain, terrain_pos):
-    if key == keyboard.Key.left:
-        terrain_pos = max(0, terrain_pos - 1)
-    elif key == keyboard.Key.right:
-        terrain_pos = min(len(terrain), terrain_pos + 1)
-    
-    terrain_draw(slice_terrain(terrain, terrain_pos))
+    if key in (keyboard.Key.left, keyboard.Key.right):
+        if key == keyboard.Key.left:
+            terrain_pos = max(0, terrain_pos - 1)
+        elif key == keyboard.Key.right:
+            terrain_pos = min(len(terrain), terrain_pos + 1)
 
-    return terrain_pos 
+        temp = slice_terrain(terrain, terrain_pos)
+
+        # Ignore for now
+        simulate_key_press([keyboard.Key.ctrl, 'a'])
+        time.sleep(0.1)
+        simulate_key_press([keyboard.Key.delete])
+        simulate_key_press('p')
+        
+        terrain_draw(temp, left_x, left_y)
+
+    return terrain_pos  
 
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
 
+def simulate_key_press(key_combination):
+    keyboard_controller = keyboard.Controller()
+    for key in key_combination:
+        keyboard_controller.press(key)
+    for key in key_combination:
+        keyboard_controller.release(key)
+
 def main():
     curr = time.time()
-    line = LineGen(10000)
+    line = LineGen(100)
     terrain = ArrayConvert(line)
     terrain_pos = 10  # Starting position
 
-    terrain_draw(slice_terrain(terrain, terrain_pos))  # Draw initial terrain slice
+    print("Place your cursor on the bottom left side of the paint application. The program will start in 3 seconds.")
+    time.sleep(3)
+
+    global left_x, left_y
+    left_x, left_y = pyautogui.position()
+
+    terrain_draw(slice_terrain(terrain, terrain_pos), left_x, left_y)
 
     def listener_on_press(key):
         nonlocal terrain_pos
