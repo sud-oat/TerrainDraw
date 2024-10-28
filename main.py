431c955cb67cb09b7a8ed01119ab0f841431fc41
carnae <samarbaskar02@gmail.com>
2024-10-28 19:50:32 +0530

    code reformatting

--- main.py
+++ main.py
@@ -3,20 +3,22 @@ import time
 from pynput import keyboard
 import pyautogui
 
+
 def LineGen(line_length):
     line = str(random.randint(2, 5))
     height = int(line)
-    
+
     for _ in range(line_length - 1):
         arr = [height] * 4
         if height < 9:
             arr.append(height + 1)
-        if height > 0: 
+        if height > 0:
             arr.append(height - 1)
-        
+
         height = random.choice(arr)
         line += str(height)
     return line
+
 
 def ArrayConvert(line):
     line_length = len(line)
@@ -25,8 +27,10 @@ def ArrayConvert(line):
         terrain[position][int(line[position])] = 1
     return terrain
 
+
 def slice_terrain(terrain, pos):
-    return terrain[max(0, pos-10):pos]
+    return terrain[max(0, pos - 10) : pos]
+
 
 def terrain_draw(terrain, left_x, left_y):
     pyautogui.moveTo(left_x, left_y + (10 - terrain[0].index(1)) * 40)
@@ -36,6 +40,7 @@ def terrain_draw(terrain, left_x, left_y):
         pyautogui.moveTo(left_x, left_y + (10 - height) * 40)
         pyautogui.mouseUp()
         left_x += 100
+
 
 def on_press(key, terrain, terrain_pos):
     if key in (keyboard.Key.left, keyboard.Key.right):
@@ -47,18 +52,20 @@ def on_press(key, terrain, terrain_pos):
         temp = slice_terrain(terrain, terrain_pos)
 
         # Ignore for now
-        simulate_key_press([keyboard.Key.ctrl, 'a'])
+        simulate_key_press([keyboard.Key.ctrl, "a"])
         time.sleep(0.1)
         simulate_key_press([keyboard.Key.delete])
-        simulate_key_press('p')
-        
+        simulate_key_press("p")
+
         terrain_draw(temp, left_x, left_y)
 
-    return terrain_pos  
+    return terrain_pos
+
 
 def on_release(key):
     if key == keyboard.Key.esc:
         return False
+
 
 def simulate_key_press(key_combination):
     keyboard_controller = keyboard.Controller()
@@ -67,13 +74,16 @@ def simulate_key_press(key_combination):
     for key in key_combination:
         keyboard_controller.release(key)
 
+
 def main():
     curr = time.time()
     line = LineGen(100)
     terrain = ArrayConvert(line)
-    terrain_pos = 10  # Starting position
+    terrain_pos = 10
 
-    print("Place your cursor on the bottom left side of the paint application. The program will start in 3 seconds.")
+    print(
+        "Place your cursor on the bottom left side of the paint application. The program will start in 3 seconds."
+    )
     time.sleep(3)
 
     global left_x, left_y
@@ -85,10 +95,13 @@ def main():
         nonlocal terrain_pos
         terrain_pos = on_press(key, terrain, terrain_pos)
 
-    with keyboard.Listener(on_press=listener_on_press, on_release=on_release) as listener:
+    with keyboard.Listener(
+        on_press=listener_on_press, on_release=on_release
+    ) as listener:
         listener.join()
-    
+
     print("Elapsed time:", time.time() - curr)
+
 
 if __name__ == "__main__":
     main()
